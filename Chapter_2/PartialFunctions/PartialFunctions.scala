2885de77050eceba21259827746940b539a129e4
James Collerton <jamescollerton@live.co.uk>
2018-01-04 07:51:30 +0000

    Got rought idea of chaining partial functions

--- Chapter_2/PartialFunctions/PartialFunctions.scala
+++ Chapter_2/PartialFunctions/PartialFunctions.scala
@@ -7,12 +7,23 @@ object PartialFunctions {
 	def definingPartialFunctions(): Unit = {
 	
 		// Note: isDefinedAt is a special function	
+		// So a partial function is defined as a function that takes in
+		// arguments, then can be undefined at an argument, even though 
+		// it is a valid input.
 		val divide = new PartialFunction[Int, Int] {
 			def apply(divisor: Int): Int = 42 / divisor
 			def isDefinedAt(divisor: Int): Boolean = if(divisor == 0) false else true 
 		}
 
-		
+		val divideInputError = new PartialFunction[Int, String] {
+			def apply(divisor: Int): String = "ERROR"
+			def isDefinedAt(divisor: Int): Boolean = if(divisor == 0) true else false 
+		}
+
+		val completeFn = divide orElse divideInputError
+
+		println("Valid " + completeFn(10))
+		println("Invalid " + completeFn(0))
 
 	}
 
