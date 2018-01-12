090255bd92cecb1214db038558e8bb72289d87ee
James Collerton <jamescollerton@live.co.uk>
2018-01-12 08:12:21 +0000

    Fiddling with for loops

---
+++ Chapter_3/ForLoops/ForLoops.scala
@@ -0,0 +1,29 @@
+object ForLoops {
+
+	def main(args: Array[String]): Unit = {
+		
+		val listOne = List("One", "Two", "Three")
+
+		// Basic generator expression
+		for(li <- listOne) println(li)
+
+		println()
+
+		// With filter
+		for(li <- listOne if(li == "One")) println(li)
+
+		// With yield
+		val listTwo = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+		val listThree = listTwo
+
+		val divisors = for {
+			liTwo <- listTwo
+			liThree <- listThree if liTwo != liThree
+			d <- Some(liTwo % liThree)
+		} yield d
+
+		for (div <- divisors.distinct) println(div)
+
+	}
+
+}
