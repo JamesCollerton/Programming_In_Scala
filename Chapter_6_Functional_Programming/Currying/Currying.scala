98aa32e9f129c0de4e847af7ee6c32b4ec8f2f9d
James Collerton <jamescollerton@live.co.uk>
2018-02-04 17:06:15 +0000

    More currying work

--- Chapter_6_Functional_Programming/Currying/Currying.scala
+++ Chapter_6_Functional_Programming/Currying/Currying.scala
@@ -4,12 +4,33 @@ object Currying {
 		a + " " + b
 	}
 
+	def multiplyThree(a: Int, b: Int, c: Int): Int = {
+		a * b * c
+	}
+
 	def main(args: Array[String]): Unit = {
 		val currConcatStr = (concatStr _).curried
 		val uncurrConcatStr = Function.uncurried(currConcatStr)
 
 		println(currConcatStr("Hello")("James"))
 		println(uncurrConcatStr("Hello", "James"))
+
+		val currMultiplication = (multiplyThree _).curried
+		val partMultiplication = currMultiplication(10)(_: Int)(_: Int)
+		val tupledMultiplication = Function.tupled(multiplyThree _)
+		val args = (2, 3, 4)
+	
+		println(currMultiplication(1)(2)(3))
+		println(partMultiplication(2, 3))
+		println(tupledMultiplication(args))
+	
+		val partialFunction: PartialFunction[(Int, Int), Int] = {
+			case (x, y) if y != 0 => x / y
+		}
+		val liftedPartialFunction = partialFunction.lift
+
+		println(liftedPartialFunction(1, 0))
+	
 	}
 
 }
