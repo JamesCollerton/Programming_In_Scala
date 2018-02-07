79890e8d9fb2f34a1ea45a85ed62dfa6f2e106a1
James Collerton <jamescollerton@live.co.uk>
2018-02-07 21:17:56 +0000

    Now need to do tail recursions

--- Chapter_6_Functional_Programming/FoldReduce/FoldReduce.scala
+++ Chapter_6_Functional_Programming/FoldReduce/FoldReduce.scala
@@ -5,7 +5,13 @@ object FoldReduce {
 	}
 
 	def reduceExample(): Unit = {
-
+		val list = List(1, 2, 3, 4, 5, 6)
+		println(list.reduce(_ + _))
+		println(list.fold(10)(_ * _))
+		
+		val listTwo = List(1, 2, 6)
+		// 6 - (2 - 1)
+		println(listTwo.reduceRight(_ - _))
 	}
 
 }
