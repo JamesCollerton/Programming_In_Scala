d4db6ff02e32deb35bc864d93ac9cfeefaad53c2
James Collerton <James.Collerton@kobaltmusic.com>
2018-02-08 16:02:52 +0000

    Wrote own fold left

--- Chapter_6_Functional_Programming/FoldReduce/FoldReduce.scala
+++ Chapter_6_Functional_Programming/FoldReduce/FoldReduce.scala
@@ -8,10 +8,18 @@ object FoldReduce {
 		val list = List(1, 2, 3, 4, 5, 6)
 		println(list.reduce(_ + _))
 		println(list.fold(10)(_ * _))
-		
+		println(foldL(list.head, list.tail)(_ + _))
+
 		val listTwo = List(1, 2, 6)
 		// 6 - (2 - 1)
 		println(listTwo.reduceRight(_ - _))
 	}
+
+	def foldL[A](value: A, seq: Seq[A])(f: (A, A) => A): A = seq match {
+		case head +: tail => foldL(f(value, head), tail)(f)
+		case _ => value
+	}
+	
+	def foldR[A, B](seq: Seq[A])(f: (A, B) => B): B = ???
 
 }
