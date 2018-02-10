baa8d411b47d76224b80dbf6f9c95fd32481b365
James Collerton <jamescollerton@live.co.uk>
2018-02-10 11:28:40 +0000

    Made the fib right recursion example

--- Chapter_6_Functional_Programming/FoldReduce/FoldReduce.scala
+++ Chapter_6_Functional_Programming/FoldReduce/FoldReduce.scala
@@ -6,14 +6,14 @@ object FoldReduce {
 
 	def reduceExample(): Unit = {
 		val list = List(1, 2, 3, 4, 5, 6)
-		println(list.reduce(_ + _))
-		println(list.fold(10)(_ * _))
-		println(foldL(list.head, list.tail)(_ + _))
+		//println(list.reduce(_ + _))
+		//println(list.fold(10)(_ * _))
+		//println(foldL(list.head, list.tail)(_ + _))
 
 		val listTwo = List(1, 2, 6)
 		// 6 - (2 - 1)
-		println(listTwo.reduceRight(_ - _))
-		println(foldR(listTwo)(_ - _))
+		//println(listTwo.reduceRight(_ - _))
+		//println(foldR(listTwo)(_ - _))
 	}
 
 	@annotation.tailrec
@@ -25,6 +25,11 @@ object FoldReduce {
 	def foldR[A](seq: Seq[A])(f: (A, A) => A): A = seq match {
 		case head +: tail if tail != Seq.empty[A] => f(head, foldR(tail)(f))
 		case head +: tail if tail == Seq.empty[A] => head 
+	}
+
+	def fibSeqGen: () => Stream[Int] = {
+		val fibs = 0 #:: 1 #:: fibs.zip(fibs.tail).map(n => n._1 + n._2)
+		fibs
 	}
 
 }
